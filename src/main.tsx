cc1cb10854ab013ebf357a7565b0fdbc2ad35bab
Vinícius <vinydias0509@gmail.com>
2025-01-10 18:52:25 -0300

    chore(deps): add and configure React Query
    - Install @tanstack/react-query
    - Install react-query-devtools for development
    - Configure QueryClientProvider in the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import './styles/global.css';
 import { App } from './App';
 
+const queryClient = new QueryClient();
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error("Elemento com ID 'root' não encontrado.");
 }
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
